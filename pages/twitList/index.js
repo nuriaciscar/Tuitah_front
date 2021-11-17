6423488b481dac5b57b61b18ed8b9f7ccebb42e8
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-17 19:53:00 +0100

    Added TwitList page with SSr rendering

--- pages/twitList/index.js
+++ pages/twitList/index.js
@@ -1,8 +1,28 @@
+import Link from "next/link";
+
 const TwitList = () => {
   return (
     <>
       <h2> Twit List :</h2>
+      <ul>
+        {tuits.map((tuit) => (
+          <li key={tuit.id}>
+            <Link href={{ pathname: "twitList/[id]", query: { id: tuit.id } }}>
+              <h3>{tuit.text}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
 export default TwitList;
+
+export const getServerSideProps = async () => {
+  const response = await fetch("https://tuits.herokuapp.com/");
+  const tuits = await response.json();
+
+  return {
+    props: { tuits },
+  };
+};

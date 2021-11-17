3892a032b53be76e9c3c3b716bd474cc2fe8e3fc
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-17 18:35:23 +0100

    Added HeadrNavMenu and the SPA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/global.css'
+import HeaderNavMenu from "@/components/HeaderNavMenu/HeaderNavMenu";
+import "../styles/global.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <HeaderNavMenu />
+      <Component {...pageProps} />
+    </>
+  );
 }
---
+++ pages/newTwit/index.js
@@ -0,0 +1,8 @@
+const NewTwit = () => {
+  return (
+    <>
+      <h2> New Twit :</h2>
+    </>
+  );
+};
+export default NewTwit;

872820cb285f4209efcc7b5abca0c710f9a89b82
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-18 00:15:11 +0100

    Styled all the pages with css

--- pages/newTwit/index.js
+++ pages/newTwit/index.js
@@ -3,7 +3,6 @@ import FormCreate from "@/components/FormCreate/FormCreate";
 const NewTwit = () => {
   return (
     <>
-      <h2>New Twit:</h2>
       <FormCreate />
     </>
   );
--- pages/twitList/index.js
+++ pages/twitList/index.js
@@ -1,15 +1,28 @@
 import Link from "next/link";
 
 const TwitList = ({ tuits }) => {
+  const getDate = (date) => {
+    const yearMonth = date.split("-");
+    const dayAndTime = yearMonth[2].split("T");
+    const time = dayAndTime[1].split(":");
+
+    return `${dayAndTime[0]}-${yearMonth[1]}-${yearMonth[0]} at ${time[0]}:${time[1]}`;
+  };
+
   return (
     <>
-      <h2>Twit List:</h2>
-      <ul>
+      <p className="twit-list"> Twits/</p>
+
+      <ul className="card">
         {tuits.map((tuit) => (
-          <li key={tuit.id}>
+          <li key={tuit.id} className="card_li">
             <Link href={{ pathname: "twitList/[id]", query: { id: tuit.id } }}>
-              <h3>{tuit.text}</h3>
+              <p>{tuit.text}</p>
             </Link>
+            <div className="card_li_info">
+              <p>Date:{getDate(tuit.date)}</p>
+              <p>Likes: {tuit.likes}</p>
+            </div>
           </li>
         ))}
       </ul>

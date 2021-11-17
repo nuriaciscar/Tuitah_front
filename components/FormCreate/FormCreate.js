61195a55b8f80803fb07a93c40e996bc5b3241c2
David Galvez <davidgalvezgomez01@gmail.com>
2021-11-17 22:21:54 +0100

    Added Form Create component

---
+++ components/FormCreate/FormCreate.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const FormCreate = () => {
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+  const initialData = {
+    text: "",
+  };
+  const [postData, setPostData] = useState(initialData);
+
+  const changeData = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.id]: event.target.value,
+    });
+  };
+  const resetForm = () => {
+    setPostData(initialData);
+  };
+  useEffect(() => {
+    setButtonDisabled(postData.text === "");
+  }, [postData]);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios.post("https://tuits.herokuapp.com/tuits", postData);
+    resetForm();
+  };
+
+  return (
+    <>
+      <div className="creation-formulary">
+        <h2>Create a new Tuit</h2>
+        <form onSubmit={onSubmit} autoComplete="off" noValidate>
+          <div className="form-group">
+            <label htmlFor="text">Text:</label>
+            <input
+              placeholder="Enter the text of the twit"
+              type="text"
+              value={postData.text}
+              className="form-control"
+              id="text"
+              onChange={changeData}
+            />
+          </div>
+          <div className="form-group">
+            <button
+              type="submit"
+              className="btn btn-info"
+              disabled={buttonDisabled}
+            >
+              Create
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default FormCreate;

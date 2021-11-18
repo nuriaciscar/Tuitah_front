8c6e073e5161f5f1091fbe0029f0681b359bef05
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-18 21:17:41 +0100

    Added function deleteTuit

--- _tests_/twitList.test.js
+++ _tests_/twitList.test.js
@@ -11,4 +11,27 @@ describe("Given a twitList component", () => {
       expect(tuit).toHaveTextContent("Twit List:");
     });
   });
+  describe("When it's invoked,", () => {
+    test("Then it should render a list of tuits.", () => {
+      const tuits = [
+        {
+          id: 1,
+          text: "Tuit 1",
+          likes: 0,
+          date: "18 MINUTES AGO",
+        },
+        {
+          id: 2,
+          text: "Test 2",
+          likes: 2,
+          date: "10 MINUTES AGO",
+        },
+      ];
+
+      render(<twitList tuits={tuits} />);
+      const result = screen.getAllByRole("listitem");
+
+      expect(result).toHaveLength(tuits.length);
+    });
+  });
 });

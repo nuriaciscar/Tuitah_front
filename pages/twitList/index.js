21f6e50f214feb3d5c9ee3b8de5b07951ad83023
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-18 18:21:16 +0100

    Added date on every tuit

--- pages/twitList/index.js
+++ pages/twitList/index.js
@@ -1,14 +1,11 @@
 import Link from "next/link";
+import ReactTimeAgo from "react-time-ago";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en.json";
+
+TimeAgo.addLocale(en);
 
 const TwitList = ({ tuits }) => {
-  const getDate = (date) => {
-    const yearMonth = date.split("-");
-    const dayAndTime = yearMonth[2].split("T");
-    const time = dayAndTime[1].split(":");
-
-    return `${dayAndTime[0]}-${yearMonth[1]}-${yearMonth[0]} at ${time[0]}:${time[1]}`;
-  };
-
   return (
     <>
       <p className="twit-list"> Twits/</p>
@@ -20,7 +17,7 @@ const TwitList = ({ tuits }) => {
               <p>{tuit.text}</p>
             </Link>
             <div className="card_li_info">
-              <p>Date:{getDate(tuit.date)}</p>
+              <ReactTimeAgo date={Date.parse(tuit.date)} locale="en" />
               <p>Likes: {tuit.likes}</p>
             </div>
           </li>

6b7ec6001dd67c3b0c24d6d9ef3109f355c0fcc9
Nuria Ciscar <nuriaciscar@MacBook-Pro-de-MacBook.local>
2021-11-18 18:27:27 +0100

    Fixed import of Header on app

--- pages/twitList/index.js
+++ pages/twitList/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/link-passhref */
 import Link from "next/link";
 import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
@@ -5,27 +6,25 @@ import en from "javascript-time-ago/locale/en.json";
 
 TimeAgo.addLocale(en);
 
-const TwitList = ({ tuits }) => {
-  return (
-    <>
-      <p className="twit-list"> Twits/</p>
+const TwitList = ({ tuits }) => (
+  <>
+    <p className="twit-list"> Twits/</p>
 
-      <ul className="card">
-        {tuits.map((tuit) => (
-          <li key={tuit.id} className="card_li">
-            <Link href={{ pathname: "twitList/[id]", query: { id: tuit.id } }}>
-              <p>{tuit.text}</p>
-            </Link>
-            <div className="card_li_info">
-              <ReactTimeAgo date={Date.parse(tuit.date)} locale="en" />
-              <p>Likes: {tuit.likes}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-};
+    <ul className="card">
+      {tuits.map((tuit) => (
+        <li key={tuit.id} className="card_li">
+          <Link href={{ pathname: "twitList/[id]", query: { id: tuit.id } }}>
+            <p>{tuit.text}</p>
+          </Link>
+          <div className="card_li_info">
+            <ReactTimeAgo date={Date.parse(tuit.date)} locale="en" />
+            <p>Likes: {tuit.likes}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </>
+);
 export default TwitList;
 
 export const getServerSideProps = async () => {
